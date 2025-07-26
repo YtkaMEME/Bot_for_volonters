ac2e9b2d13307ba5dd7e3844ee963a3782755bee
YtkaMEME <a1-6@MacBook-Air-Mira.local>
2025-07-26 14:21:25 +0300

    Исправил текст записи в google drive

--- keyboards/inline.py
+++ keyboards/inline.py
@@ -2,12 +2,12 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def direction_keyboard():
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="Волонтёры-инструкторы", callback_data="Инструкторы")],
-        [InlineKeyboardButton(text="Основные форматы", callback_data="Форматы")],
-        [InlineKeyboardButton(text="Сервисное сопровождение", callback_data="Сервис")],
-        [InlineKeyboardButton(text="Штаб / Медиа", callback_data="ШтабМедиа")],
-        [InlineKeyboardButton(text="Сопровождение событий", callback_data="События")],
-        [InlineKeyboardButton(text="Работа с участниками", callback_data="Участники")],
-        [InlineKeyboardButton(text="Работа со зрителями и аккредитация", callback_data="Зрители")],
+        [InlineKeyboardButton(text="Волонтеры-инструкторы", callback_data="Волонтеры-инструкторы")],
+        [InlineKeyboardButton(text="Основные форматы", callback_data="Основные форматы")],
+        [InlineKeyboardButton(text="Сервисное сопровождение", callback_data="Сервисное сопровождение")],
+        [InlineKeyboardButton(text="Штаб/Медиа", callback_data="Штаб/Медиа")],
+        [InlineKeyboardButton(text="Сопровождение событий", callback_data="Сопровождение событий")],
+        [InlineKeyboardButton(text="Работа с участниками", callback_data="Работа с участниками")],
+        [InlineKeyboardButton(text="Работа со зрителям и аккредитация", callback_data="Работа со зрителям и аккредитация")],
     ])
     return keyboard
